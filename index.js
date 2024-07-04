d8c8697de1e77d5f1dbe619f47aa36dd6ea5c09d
Josiah <josiahburnette02@gmail.com>
2024-07-04 16:10:57 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 let cash = document.getElementById('cash');
 let click = document.getElementById('click');
 let x = click.addEventListener('click', check);
+let price = 1.87;
+let cid = [
+  ['PENNY', 1.01],
+  ['NICKEL', 2.05],
+  ['DIME', 3.1],
+  ['QUARTER', 4.25],
+  ['ONE', 90],
+  ['FIVE', 55],
+  ['TEN', 20],
+  ['TWENTY', 60],
+  ['ONE HUNDRED', 100]
+];
+
 function check() {
     cash
     console.log(cash.value)
