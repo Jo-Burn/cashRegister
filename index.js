758b19c52a6d372bf94c39b6469b8364ccadeac8
Josiah <josiahburnette02@gmail.com>
2024-07-04 15:08:57 -0700

    Added JS file

---
+++ index.js
@@ -0,0 +1,7 @@
+let cash = document.getElementById('cash');
+let click = document.getElementById('click');
+let x = click.addEventListener('click', check);
+function check() {
+    cash
+    console.log(cash.value)
+}
